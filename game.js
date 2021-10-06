341f4cfa67df44669aff74967270533a406c7ba8
victorGamer53 <87991198+victorGamer53@users.noreply.github.com>
2021-10-06 08:29:26 +0100

    Update game.js

--- game.js
+++ game.js
@@ -1,13 +1,5 @@
-var stat = new stats();
-var points_a = stat.points_a;
-var production_a = stat.production_a;
-function stats()
-{
-  
-  this.points_a = new OmegaNum(0)
-  this.production_a = new OmegaNum(0)
-  
-  
+var stats = {
+alphabet:{a:{a_points: new OmegaNum(0),a_production: new OmegaNum(0)}}
 }
 function saveUser(Stats)
 {
@@ -16,14 +8,14 @@ function saveUser(Stats)
 }
 function loadGame()
 {
-  stat = JSON.parse(localStorage.getItem("HJSGame"))
+  stats = JSON.parse(localStorage.getItem("HJSGame"))
 }
 setTimeout(function(){loadGame()},0)
 function add()
-{
+{ 
   setTimeout(add,1)
-  points_a = points_a.add(1)
-  document.getElementById("a_text").innerHTML = points_a.toFixed(3);
+  stats.alphabet.a.a_points = stats.alphabet.a.a_points.add(1)
+  document.getElementById("a_text").innerHTML = stats.alphabet.a.a_points.toFixed(3);
 }
 saveUser(stat)
 add()
